bc365ed60679e2b3ea8d958d2a88b7430408410a
Luis Goyes <luis.goyes117@gmail.com>
2021-03-08 20:10:16 -0500

    Working on login view

--- TongoNews/Auth/Login/LoginView.swift
+++ TongoNews/Auth/Login/LoginView.swift
@@ -12,12 +12,60 @@ protocol LoginViewType {
 }
 
 final class LoginView: UIView, LoginViewType {
+    
+    struct Constants {
+        static let email = "Email"
+        static let password = "Password"
+    }
+    
+    lazy var mainStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        return stackView
+    }()
+    
+    lazy var emailField: UITextField = {
+        let emailField = UITextField()
+        emailField.placeholder = Constants.email
+        return emailField
+    }()
+    
+    lazy var passwordField: UITextField = {
+        let passwordField = UITextField()
+        passwordField.placeholder = Constants.password
+        return passwordField
+    }()
+    
+    var subviewsAreConfigured = false
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor = .red
+        configureSubviews()
     }
     
     required init?(coder: NSCoder) {
         fatalError()
+    }
+    
+    func configureSubviews() {
+        setMainContainer(mainStackView)
+        addField(emailField, to: mainStackView)
+        addField(passwordField, to: mainStackView)
+        subviewsAreConfigured = true
+    }
+    
+    func setMainContainer(_ stackView: UIStackView) {
+        self.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: self.layoutMarginsGuide.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: self.layoutMarginsGuide.bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: self.layoutMarginsGuide.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: self.layoutMarginsGuide.trailingAnchor),
+        ])
+    }
+    
+    func addField(_ textField: UITextField, to stackView: UIStackView) {
+        stackView.addArrangedSubview(textField)
     }
 }
--- TongoNewsTests/Auth/LoginViewTests.swift
+++ TongoNewsTests/Auth/LoginViewTests.swift
@@ -5,23 +5,82 @@
 //  Created by Luis Goyes Garces on 3/03/21.
 //
 
+import UIKit
 import XCTest
 @testable import TongoNews
 
 class LoginViewTests: XCTestCase {
     var sut: LoginView!
-
+    
     override func setUpWithError() throws {
         try super.setUpWithError()
         sut = LoginView()
     }
-
+    
     override func tearDownWithError() throws {
         sut = nil
         try super.tearDownWithError()
     }
     
-    func testWhenInit_ThenSetBackgroundColorToRed() {
-        XCTAssertTrue( sut.backgroundColor == .red )
+    func testWhenInit_ThenInvokeConfigureSubviews() {
+        XCTAssertTrue( sut.subviewsAreConfigured )
+    }
+    
+    func test_WhenSetMainContainerIsCalled_ThenAddStackviewAsSubview() {
+        let mainStackView = UIStackView()
+        sut.setMainContainer(mainStackView)
+        let mainStackViewIsSubview = sut.subviews.contains(mainStackView)
+        XCTAssertTrue(mainStackViewIsSubview)
+    }
+    
+    func test_WhenSetMainContainerIsCalled_ThenResetTranslatesAutoresizingMaskIntoConstraints() {
+        let mainStackView = UIStackView()
+        sut.setMainContainer(mainStackView)
+        XCTAssertFalse(mainStackView.translatesAutoresizingMaskIntoConstraints)
+    }
+    
+    func test_WhenConfigureSubViewsIsCalled_ThenSetTheMainStackViewAsSubview() {
+        sut.configureSubviews()
+        
+        let mainStackView = sut.mainStackView
+        let mainStackViewIsSubview = sut.subviews.contains(mainStackView)
+        XCTAssertTrue(mainStackViewIsSubview)
+    }
+    
+    func test_WhenAddFieldIsCalled_ThenAddFieldToTheMainStackView() {
+        let mainStackView = UIStackView()
+        
+        let textField = UITextField()
+        sut.addField(textField, to: mainStackView)
+        
+        XCTAssertTrue(mainStackView.arrangedSubviews.contains(textField))
+    }
+    
+    func test_WhenConfigureSubViewsIsCalled_ThenSetTheEmailFieldAsTheFirstSubviewInTheStack() {
+        sut.configureSubviews()
+        
+        let mainStackView = sut.mainStackView
+        let emailField = sut.emailField
+        
+        XCTAssertTrue(mainStackView.subviews[0] === emailField)
+    }
+    
+    func test_WhenConfigureSubViewsIsCalled_ThenSetThePasswordFieldAsTheSecondSubviewInTheStack() {
+        sut.configureSubviews()
+        
+        let mainStackView = sut.mainStackView
+        let passwordField = sut.passwordField
+        
+        XCTAssertTrue(mainStackView.subviews[1] === passwordField)
+    }
+    
+    func test_WhenInit_SetPlaceHolderToEmailField() {
+        let emailField = sut.emailField
+        XCTAssertEqual(emailField.placeholder, LoginView.Constants.email)
+    }
+    
+    func test_WhenInit_SetPlaceHolderToPasswordField() {
+        let passwordField = sut.passwordField
+        XCTAssertEqual(passwordField.placeholder, LoginView.Constants.password)
     }
 }
